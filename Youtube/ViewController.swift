591df88ecf09aecefdbe5fa4745e7ffe19853f79
tmtuan <tranminhtuan684@gmail.com>
2017-07-02 08:34:41 +0700

    Initial project

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Tran Minh Tuan on 7/2/17.
+//  Copyright © 2017 Tran Minh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
