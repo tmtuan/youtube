761a8c920cacbfa3337dce614167b05aa77489ef
tmtuan <tranminhtuan684@gmail.com>
2017-07-02 09:19:42 +0700

    Get cells in collectionView

--- Youtube/HomeController.swift
+++ Youtube/HomeController.swift
@@ -16,9 +16,22 @@ class HomeController: UICollectionViewController {
         
         navigationItem.title = "Home"
         collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    
     }
 
-    
+    // MARK: - UICollectionView methods
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
 
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
 }
 
