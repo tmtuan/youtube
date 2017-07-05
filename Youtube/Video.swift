65069535d0734a206ad02aa6686301db648e88d1
tmtuan <tranminhtuan684@gmail.com>
2017-07-05 19:54:48 +0700

    Create Video class as a model

---
+++ Youtube/Video.swift
@@ -0,0 +1,15 @@
+//
+//  Video.swift
+//  Youtube
+//
+//  Created by Tran Minh Tuan on 7/5/17.
+//  Copyright © 2017 Tran Minh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class Video: NSObject {
+    
+    var thumbnailImageName: String?
+    var title: String?
+}
